1956882ffb4b5b1527fd6e09314bbc13d1bf6362
JOLee83 <lee.justin.oliver@gmail.com>
2018-10-09 20:03:58 -0400

    all done

---
+++ src/Movies.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import Data from './Data'
+import styled from 'styled-components'
+
+const Header = styled.header`
+	display: flex;
+	justify-content: center;
+	text-align: center;
+	h1 {
+		color: aqua;
+		text-shadow: 0.1rem 0.1rem 0 rgb(233, 25, 164);
+		background-color: rgb(0, 255, 0);
+		border: 0.4rem solid rgb(248, 0, 186);
+		border-radius: 5px;
+		font-family: 'Press Start 2P', cursive;
+		padding: 1rem;
+	}
+`
+const Movie = styled.section`
+	width: 500px;
+	margin: 1rem;
+	padding: 1rem;
+	text-align: center;
+	color: aqua;
+	text-shadow: 0.1rem 0.1rem 0 rgb(233, 25, 164);
+	background-color: rgb(0, 255, 0);
+	border: 0.4rem solid rgb(248, 0, 186);
+	border-radius: 5px;
+	aside {
+		display: flex;
+		justify-content: center;
+	}
+	p {
+		padding: 1rem;
+		margin-left: 1rem;
+    text-align: left;
+    font-family: 'VT323', monospace;
+    font-size: 1.3rem;
+	}
+	img {
+		border-radius: 5px;
+		padding: 0.4rem;
+		background-color: rgb(248, 0, 186);
+	}
+	h2 {
+    font-family: 'Press Start 2P', cursive;
+    font
+	}
+`
+
+class Movies extends Component {
+	render() {
+		return (
+			<>
+				<Header>
+					<h1>1989 MOVIES</h1>
+				</Header>
+				<main>
+					{Data.results.map(result => {
+						return (
+							<Movie key={result.title}>
+								<h2>{result.title}</h2>
+								<aside>
+									<img
+										alt={result.title}
+										src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${
+											result.poster_path
+										}`}
+									/>
+									<p>{result.overview}</p>
+								</aside>
+							</Movie>
+						)
+					})}
+				</main>
+			</>
+		)
+	}
+}
+
+export default Movies
